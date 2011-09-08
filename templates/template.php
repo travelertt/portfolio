97ac4ed6d056e0072444db84e87008122146d387
Tory Trone <travelertt@gmail.com>
2011-09-07 19:06:11 -0700

    Removed the taxonomy preprocessor
    Removed the taxonomy preprocessor that added the "+" in-between terms.
    Also did some house cleaning for the images.  Also, added and tpl.php
    for articles.

--- templates/template.php
+++ templates/template.php
@@ -1,10 +1,2 @@
 <?php
-/**
- * Alter the appearance of the taxonomy terms
- */
 
-function my_custom_theme_entity_view_alter(&$build, $type) {
-  if ($build['#view_mode'] == 'full' && $type == 'taxonomy') {
-    dprint_r($build);
-  }
-}
